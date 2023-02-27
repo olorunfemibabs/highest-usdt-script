fd1ccba1b8ec9bcbdca9d1375e94b8c7fbb1bc14
Olorunfemi Babalola <olorunfemisamuel01@gmail.com>
2023-02-27 08:09:40 +0100

    commit

---
+++ scripts/highestAmount.ts
@@ -0,0 +1,21 @@
+import { Contract, ethers } from 'ethers';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+async function getHighestHolder() {
+  const response = await fetch(
+    'https://api.ethplorer.io/getTopTokenHolders/0xdAC17F958D2ee523a2206206994597C13D831ec7?apiKey=freekey&limit=1'
+  );
+  const data = await response.json();
+  const highestHolder = data.holders[0];
+  console.log(`highest holder address of usdt is ${highestHolder}`);
+  return highestHolder;
+
+}
+
+
+getHighestHolder().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
